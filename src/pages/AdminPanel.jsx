8acc8c379bd8e767d635dbc5903dd6f57613bde8
piyushdev-sh <piyush94678@gmail.com>
2026-03-04 17:32:36 +0530

    feat: Clean up AdminPanel by removing unused imports

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -1,12 +1,6 @@
-import React from 'react'
 import AdminSideSection from '../sections/AdminSideSection'
 import { Outlet } from 'react-router'
-import AdminDashboard from './AdminDashboard'
-import AdminProductList from './AdminProductList'
-import AdminProduct from './AdminProduct'
-import AdminCatogeries from './AdminCatogeries'
-import AdminOrder from './AdminOrder'
-import AdminSetting from './AdminSetting'
+
 
 const AdminPanel = () => {
   return (
